c7fec88d067333d554a0070896af2e3d93de1c39
Darren Gegantino <darren@mergecommit.com>
2018-07-01 00:30:42 +0800

    ✨ Add rest endpoint: list users

--- test/travenger_web/controllers/user_controller_test.exs
+++ test/travenger_web/controllers/user_controller_test.exs
@@ -15,6 +15,27 @@ defmodule TravengerWeb.UserControllerTest do
     }
   end
 
+  describe "index/2" do
+    test "returns a paginated list of users", %{conn: conn} do
+      conn = get(conn, user_path(conn, :index))
+      %{"data" => data} = json_response(conn, :ok)
+
+      assert data["page_number"]
+      assert data["page_size"]
+      assert data["total_entries"]
+      assert data["total_pages"]
+      refute data["entries"] == []
+    end
+
+    test "returns a paginated list of users given gender", %{conn: conn} do
+      params = %{gender: "male"}
+      conn = get(conn, user_path(conn, :index), params)
+      %{"data" => data} = json_response(conn, :ok)
+
+      assert data["total_entries"] == 1
+    end
+  end
+
   describe "show/2" do
     test "returns a user given id", %{conn: conn, user: user}do
       conn = get(conn, user_path(conn, :show, user.id))
