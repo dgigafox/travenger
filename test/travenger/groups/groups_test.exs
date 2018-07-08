dcc44d0085504ccc615da0c9da960622e0d0af4e
Darren Gegantino <darren@mergecommit.com>
2018-07-08 12:45:15 +0800

    ✅ Add test for create group functionality

--- test/travenger/groups/groups_test.exs
+++ test/travenger/groups/groups_test.exs
@@ -1,3 +1,18 @@
 defmodule Travenger.GroupsTest do
   use Travenger.DataCase
+
+  import Travenger.Factory
+
+  alias Travenger.Groups
+
+  describe "create_group/1" do
+    test "return a created group" do
+      {:ok, group} = Groups.create_group(params_for(:group))
+
+      assert group.id
+      assert group.name
+      assert group.image_url
+      assert group.description
+    end
+  end
 end
