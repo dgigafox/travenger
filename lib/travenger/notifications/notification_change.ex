5ff846269ce79df9cfeb6e61f2808282817983d8
Darren Gegantino <darren@mergecommit.com>
2018-09-14 19:29:29 +0800

    🗃 Create notification_change schema

---
+++ lib/travenger/notifications/notification_change.ex
@@ -0,0 +1,20 @@
+defmodule Travenger.Notifications.NotificationChange do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Travenger.Accounts.User
+  alias Travenger.Notifications.NotificationObject
+
+  schema "notification_changes" do
+    belongs_to(:notification_object, NotificationObject)
+    belongs_to(:actor, User)
+    timestamps()
+  end
+
+  @doc false
+  def changeset(notification_change, attrs \\ %{}) do
+    notification_change
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
