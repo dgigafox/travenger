034ffff9c92bcd1b11d515536dad96631f9e18ee
Darren Gegantino <darren@mergecommit.com>
2018-10-13 11:13:07 +0800

    🗃 Add deleted at field in groups

--- lib/travenger/groups/group.ex
+++ lib/travenger/groups/group.ex
@@ -22,6 +22,8 @@ defmodule Travenger.Groups.Group do
     field(:image_url, :string)
     field(:description, :string)
 
+    field(:deleted_at, :naive_datetime)
+
     belongs_to(:user, User)
     has_many(:members, Membership)
     has_many(:events, Event)
