5a8f49df23fd43c77bd9e32a2f98ba89fac6649c
Darren Gegantino <darren@mergecommit.com>
2018-10-14 12:23:48 +0800

    ✨ Add find_membership by roles

--- lib/travenger/helpers/queries/membership.ex
+++ lib/travenger/helpers/queries/membership.ex
@@ -7,4 +7,10 @@ defmodule Travenger.Helpers.Queries.Membership do
   def where_admin(query, _params) do
     where(query, [q], q.role == ^:creator or q.role == ^:admin)
   end
+
+  def where_roles(query, %{roles: roles}) when is_list(roles) do
+    where(query, [q], q.role in ^roles)
+  end
+
+  def where_roles(query, _), do: query
 end
